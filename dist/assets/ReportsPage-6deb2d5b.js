cd8d134a9dda41b68743264e2df1b1d520a2a7d4
Kevin Loquencio <kevin.loquencio@gmail.com>
2023-10-08 11:43:50 +0800

    rebuild

--- dist/assets/ReportsPage-6deb2d5b.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./_plugin-vue_export-helper-c27b6911.js";import{o,b as r}from"./index-de386e80.js";const t={};function c(n,s){return o(),r("h1",null,"Soon")}const f=e(t,[["render",c]]);export{f as default};
