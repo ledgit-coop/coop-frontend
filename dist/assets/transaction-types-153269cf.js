39481cc3f93c12784466146c486df441b17f8d63
Kevin Loquencio <kevin.loquencio@gmail.com>
2023-12-12 10:09:32 +0800

    rebuild

---
+++ dist/assets/transaction-types-153269cf.js
@@ -0,0 +1,1 @@
+var I=(E=>(E.EXPENSES="expense",E.REVENUE="revenue",E.RECEIVABLES="receivable",E.PAYABLES="payable",E.LIABILITIES="liability",E))(I||{});export{I as T};
--- dist/assets/transaction-types-583c45c1.js
+++
@@ -1,1 +0,0 @@
-var e=(E=>(E.EXPENSES="expenses",E.REVENUE="revenues",E.RECEIVABLES="receivables",E.PAYABLES="payables",E.LIABILITIES="liabilities",E))(e||{});export{e as T};
